c2a537687c25a34fd24a91c5521351f0e4c02db2
Ayush <dayush0524@gmail.com>
2026-01-23 23:19:34 +0530

    adding users-table

--- app/tables.py
+++ app/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table,Column,Integer,Text,MetaData,TIMESTAMP,CheckConstraint
+from sqlalchemy import Table,Column,Integer,Text,MetaData,TIMESTAMP,CheckConstraint,UUID,Boolean
 from sqlalchemy.sql import func
 
 metadata=MetaData()
@@ -15,4 +15,16 @@ tasks=Table(
     CheckConstraint("status IN ('pending','in_progress','completed')"),
     CheckConstraint("length(trim(description))>0",name="tasks_description_not_empty"),
     CheckConstraint("length(trim(title))>0",name="tasks_title_not_empty")
+)
+
+users=Table(
+    "users",
+    metadata,
+    Column("id",UUID(as_uuid=True),server_default=func.uuid_generate_v4(),primary_key=True),
+    Column("email",Text,nullable=False,unique=True),
+    Column("hashed_password",Text,nullable=False),
+    Column("is_active",Boolean,nullable=False,server_default="true"),
+    Column("created_at", TIMESTAMP(timezone=True),nullable=False, server_default=func.now()),
+    Column("updated_at", TIMESTAMP(timezone=True),nullable=False, server_default=func.now())
+    
 )
